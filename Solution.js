d27192ecf0b603d0a983d8abbd26e7cd2a9f2617
Lachezar Kutsarov <lkutsarov@gmail.com>
2024-07-10 20:02:57 +0200

    Create Solution.js

---
+++ Solution.js
@@ -0,0 +1,20 @@
+
+/**
+ * @param {string[]} logs
+ * @return {number}
+ */
+var minOperations = function (logs) {
+
+    let minOperationsToReturnToMainFolder = 0;
+    for (let log of logs) {
+        const isChildFolder = log.charAt(0) !== '.';
+        const isParentFolder = log.charAt(1) === '.';
+
+        if (isChildFolder) {
+            ++minOperationsToReturnToMainFolder;
+        } else if (minOperationsToReturnToMainFolder > 0 && isParentFolder) {
+            --minOperationsToReturnToMainFolder;
+        }
+    }
+    return minOperationsToReturnToMainFolder;
+};
